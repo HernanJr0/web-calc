e7fad764c161b416e1520d2d309ba128d9ed2e51
wMzley <wesleyfelipe1904@gmail.com>
2024-03-18 23:04:16 -0400

    ksdksd

--- src/components/Keyboard/Botao.tsx
+++ src/components/Keyboard/Botao.tsx
@@ -6,26 +6,9 @@ type Props = {
     children: string
 }
 
-var expressao = document.querySelector("#exp")
-
-<<<<<<< HEAD
-function Botao({ children }: Props | any) {
-=======
-    breakpoints: {
-        values: {
-            xs: 0, // phone
-            sm: 300, // tablets
-            md: 520, // small laptop
-            lg: 900, // desktop
-            xl: 1200 // large screens
-        },
-    },
-});
-
 // const expression = document.querySelector("#exp")
 
 function Botao({ children }: Props) {
->>>>>>> 6ab0d917ff823a6dcf89d187cd53ff7cf7d783fa
 
     return (
         <Button
